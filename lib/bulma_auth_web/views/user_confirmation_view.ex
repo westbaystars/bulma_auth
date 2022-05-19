37c920322f6964e24759b47a9a928ed8797ffc4a
Michael Westbay <westbaystars@gmail.com>
2022-05-19 14:03:54 +0900

    Changed the user confirmation send and confirm pages to use SurfaceBulma components.

--- lib/bulma_auth_web/views/user_confirmation_view.ex
+++ lib/bulma_auth_web/views/user_confirmation_view.ex
@@ -1,3 +1,40 @@
 defmodule BulmaAuthWeb.UserConfirmationView do
   use BulmaAuthWeb, :view
+
+  alias SurfaceBulma.{Title, Form, Link}
+  alias SurfaceBulma.Form.{EmailInput, Submit}
+
+  def render("edit.html", assigns) do
+    ~F"""
+    <Title size="1">Confirm account</Title>
+
+    <Form for={:user} action={"/users/confirm/#{@token}"} method="POST" :let={form: _f}>
+      <Submit type="submit" color="primary">Confirm my account</Submit>
+    </Form>
+
+    <p>
+      <Link href="/users/register">Register</Link> |
+      <Link href="/users/log_in">Log in</Link>
+    </p>
+    """
+  end
+
+  def render("new.html", assigns) do
+    ~F"""
+    <Title size="1">Resend confirmation instructions</Title>
+
+    <Form for={:user} action="/users/confirm/" method="POST" :let={form: f}>
+
+      <!-- :email field -->
+      <EmailInput form={f} field={:email} icon_left="envelope" opts={[required: true]} placeholder="Your e-mail address"/>
+
+      <Submit type="submit" color="primary">Confirm my account</Submit>
+    </Form>
+
+    <p>
+      <Link href="/users/register">Register</Link> |
+      <Link href="/users/log_in">Log in</Link>
+    </p>
+    """
+  end
 end
